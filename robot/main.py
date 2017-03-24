e95366f853864f08ae78c13beb815d935de2b39d
KhalilWang <hsgwpj@gmail.com>
2017-03-24 21:34:55 +0800

    Err: flask-page cannot show well

--- robot/main.py
+++ robot/main.py
@@ -2,12 +2,17 @@
 import flask
 from robot import robot
 from werobot.contrib.flask import make_view
+import sys
+import os
 
+basedir = os.path.dirname(os.path.abspath(__file__))
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 
 app = flask.Flask(__name__)
 app.add_url_rule(
-    rule = '/robot/',
+    rule = '/robot',
     endpoint = 'werobot',
     view_func = make_view(robot),
     methods = ['GET', 'POST']
@@ -15,7 +20,12 @@ app.add_url_rule(
 
 @app.route('/')
 def index():
-  return 'helloWorld! <br/>'
+    return 'helloWorld'
+
+@app.route('/student')
+def student():
+    name = flask.request.args.get(u'name', 'Stranger')
+    return 'Welcome, ' + name + '!'
 
 if __name__ == '__main__':
   app.run(host = '0.0.0.0', port = 80)
--- robot/robot.py
+++ robot/robot.py
@@ -13,39 +13,39 @@ conf = werobot.config.Config()
 conf.from_pyfile('config.py')
 client = werobot.client.Client(conf)
 
-rst = client.create_menu({
-                "button":[
+client.create_menu({
+        "button":[
+            {
+                "type":"click",
+                "name":"我是学生",
+                "key":"STUDENT_KEY"
+            },
+            {
+                "type":"click",
+                "name":"我是老师",
+                "key":"TEACHER_KEY"
+            },
+            {
+                "name":"我的信息",
+                "sub_button":[
                     {
-                        "type":"click",
-                        "name":"我是学生",
-                        "key":"STUDENT_KEY"
+                        "type":"view",
+                        "name":"搜索",
+                        "url":"http://www.soso.com/"
+                    },
+                    {
+                        "type":"view",
+                        "name":"视频",
+                        "url":"http://v.qq.com/"
                     },
                     {
                         "type":"click",
-                        "name":"我是老师",
-                        "key":"TEACHER_KEY"
-                    },
-                    {
-                        "name":"我的信息",
-                        "sub_button":[
-                            {
-                                "type":"view",
-                                "name":"搜索",
-                                "url":"http://www.soso.com/"
-                            },
-                            {
-                                "type":"view",
-                                "name":"视频",
-                                "url":"http://v.qq.com/"
-                            },
-                            {
-                                "type":"click",
-                                "name":"赞一下我们",
-                                "key":"GOOD"
-                            }
-                        ]
+                        "name":"赞一下我们",
+                        "key":"GOOD"
                     }
-                ]})
+                ]
+            }
+        ]})
 
 
 
@@ -57,42 +57,36 @@ rst = client.create_menu({
 
 @robot.key_click('STUDENT_KEY')
 def student_key():
-    return '你按下了我是学生按钮!'
+    return u'你按下了我是学生按钮!'
 
+
+@robot.key_click('TEACHER_KEY')
+def teacher_key():
+    return u'233'
+
+
+@robot.filter(u"我是学生")
+def student(message, session):
+    session['status'] = 'input_student_name'
+    return u'请输入你的名字!'
 
 @robot.text
-def switch_text_msg(message, session):
-  msg = message.content
-  print 'msg :' + msg
-
-  if re.compile('*网址*').match(msg):
-    return 'http://khalilwang.tech'
-
-  if msg == '我是学生':
-
-    session['user'] = 'student'
-    session['status'] = 'input_name'
-    return '请输入你的名字'
-  elif session['status'] == 'input_name':
-    session['name'] = msg
-    session['status'] = 'none'
-    return msg + '同学, 欢迎!'
+def check_input(message, session):
+    if session['status'] == 'input_student_name':
+        name = message.content
+        session['status'] = ''
+        return [
+            u'欢迎' + name,
+            u'点击这里开始提交',
+            'img',
+            u'khalilwang.tech/student?name=' + name
+        ]
 
 
-  elif msg == '我是老师':
-    session['user'] = 'teacher'
-    session['status'] = 'input_teacher_code'
-    return '请输入老师密码!'
-  elif session['status'] == 'input_teacher_code':
-    # sql judgement
-    session['status'] = 'none'
-    return '你的密码是:' + msg
-  else:
-    return '学生请输入: 我是学生 \n老师请输入: 我是老师\n'
 
 @robot.subscribe
 def subscribe(message):
-  return '欢迎关注点点!\n学生请输入: 我是学生\n老师请输入: 我是老师!\n感谢您的使用!'
+  return u'欢迎关注!'
 
 
 #robot.config['HOST'] = '0.0.0.0'
